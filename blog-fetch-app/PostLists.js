fcc561f6513603c65ff03721961941427a403ee7
Alutta <Aluttamatambo5@gmail.com>
2024-11-24 09:30:25 +0200

    Added const with return

---
+++ blog-fetch-app/PostLists.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostsList = ({ posts }) => {
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
